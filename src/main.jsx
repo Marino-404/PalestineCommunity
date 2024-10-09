68d97687e78853bd45dc68be5c58766f2511d34e
Juan Marino Rova <maarino89@hotmail.com>
2024-10-09 04:00:46 -0300

    1.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-import { AppProvider } from './AppContext'; 
-import App from './App';
-import BackGroundPage from './assets/BackGroundPage';
+import { AppProvider } from './AppContext.jsx'; 
+import App from './App.jsx';
+import BackGroundPage from './assets/BackGroundPage.jsx';
 import './index.css'; 
-import AnimatedLines from './assets/animatedLines';
+import AnimatedLines from './assets/animatedLines.jsx';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
