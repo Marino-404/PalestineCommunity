c7136601a50b9ea2e4c353b748574dc81c075b23
Juan Marino Rova <maarino89@hotmail.com>
2024-10-17 10:07:21 -0300

    1.0

--- src/pages/CardDetail1.jsx
+++ src/pages/CardDetail1.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAppContext } from '../AppContext.jsx';
 import Back from '../assets/Back';
 import PhotoMovie from '../assets/PhotoMovie';
 import DivLineCards from '../assets/DivLineCards';
@@ -7,10 +8,19 @@ import { div } from 'framer-motion/client';
 import img1 from "/src/images/peliculas/pelicula1.png"
 import img2 from "/src/images/peliculas/pelicula2.png"
 import img3 from "/src/images/peliculas/pelicula3.png"
+import { FaStarHalfAlt } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
+import { FaPlayCircle } from "react-icons/fa";
 
-export const SectionStyle = 'flex flex-col xl:flex-row justify-center items-center xl:justify-start xl:items-start md:items-start md:justify-start ';
+
+export const SectionStyle = 'flex flex-col xl:flex-row justify-center items-center xl:justify-center xl:items-start md:items-start md:justify-start ';
+export const TitleStyle = 'font-poppins font-bold text-3xl pt-4 xl-pt-0'
+export const DescriptionStyle = 'font-poppins font-normal text-xl py-2 px-8'
 
 const CardDetail1 = () => {
+
+  const {mode} = useAppContext();
 
   return (
    <>
@@ -18,19 +28,85 @@ const CardDetail1 = () => {
       <Back SectionName={'Peliculas'} />
     </div>
 
-    <div className='w-[98%] xl:w-[68%] xl:mx-auto md:mx-auto flex flex-col' >
+    <div className={`w-[98%] xl:w-[68%] xl:mx-auto md:mx-auto flex flex-col ${ mode ? 'text-[#ffffffc2]' : 'text-[#000000c2]'} z-50`} >
       <DivLineCards />
-      <section className={SectionStyle} >
-        <PhotoMovie img={img2} />
-      </section>
+
+      <div className='flex xl:flex-row flex-col' >
+        <section className={SectionStyle} >
+          <PhotoMovie img={img2} />
+        </section>
+        <div className='flex flex-col text-center justify-center items-center gap-6' >
+          <h1 className={TitleStyle} >
+            A 200 Metros
+          </h1>
+          <span className={DescriptionStyle} >
+            Mustafa y su esposa Salwa provienen de dos aldeas palestinas que están a solo 200 metros de distancia, pero separadas por el muro. Su situación de vida inusual está comenzando a afectar su matrimonio feliz, pero la pareja hace lo que puede para que funcione. 
+          </span>
+          <div className='flex flex-row text-xl' >
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStarHalfAlt />
+          </div>
+          <a href='https://www.netflix.com/ar/title/81443440?source=35&preventIntent=true' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+            <FaPlayCircle />
+          </a>
+        </div>
+      </div>
+
       <DivLineCards />
-      <section className={SectionStyle} >
-        <PhotoMovie img={img1} />
-      </section>
+
+      <div className='flex xl:flex-row flex-col' >
+        <section className={SectionStyle} >
+          <PhotoMovie img={img1} />
+        </section>
+        <div className='flex flex-col text-center justify-center items-center gap-6' >
+          <h1 className={TitleStyle} >
+            La sal de este mar
+          </h1>
+          <span className={DescriptionStyle} >
+            Soraya tiene veintiocho años. Nació y se educó en Brooklyn, pero ha decidido regresar a Palestina, país del que su familia tuvo que exiliarse en 1948. Desde su llegada a Ramallah, intenta recuperar el dinero de sus abuelos. Por su parte Emad, nacido en los territorios palestinos, ha conocido toda su vida la ocupación y la realidad palestina, y tiene como máximo sueño obtener un visado para ir a estudiar a Canadá y marcharse de allí.  
+          </span>
+          <div className='flex flex-row text-xl' >
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStarHalfAlt />
+            <FaRegStar />
+          </div>
+          <a href='https://www.netflix.com/title/70117041?preventIntent=true' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+            <FaPlayCircle />
+          </a>
+        </div>
+      </div>
+
       <DivLineCards />
-      <section className={SectionStyle} >
-        <PhotoMovie img={img3} />
-      </section>
+
+      <div className='flex xl:flex-row flex-col' >
+        <section className={SectionStyle} >
+          <PhotoMovie img={img3} />
+        </section>
+        <div className='flex flex-col text-center justify-center items-center gap-6' >
+          <h1 className={TitleStyle} >
+            Nacido en GAZA
+          </h1>
+          <span className={DescriptionStyle} >
+            Rodada durante la última ofensiva israelí que asoló la Franja de Gaza, 2014. Nacido en Gaza sigue a diez niños que nos cuentan cómo es su día a día bajo las bombas y el embargo.
+          </span>
+          <div className='flex flex-row text-xl' >
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaRegStar />
+          </div>
+          <a href='https://www.justwatch.com/mx/pelicula/nacido-en-gaza' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+            <FaPlayCircle />
+          </a>
+        </div>
+      </div>
+
       <DivLineCards />
       <Footer />
     </div>
--- src/pages/CardDetail2.jsx
+++ src/pages/CardDetail2.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useAppContext } from '../AppContext';
 import Back from '../assets/Back';
 import PhotoMovie from '../assets/PhotoMovie';
 import DivLineCards from '../assets/DivLineCards';
 import Footer from '../components/Footer';
-import { SectionStyle } from './CardDetail1';
+import { SectionStyle, TitleStyle, DescriptionStyle } from './CardDetail1';
+import { FaStarHalfAlt } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa";
+import { FaPlayCircle } from "react-icons/fa";
 
 import img1 from "/src/images/series/serie1.png"
 import img2 from "/src/images/series/serie2.png"
@@ -15,42 +20,200 @@ import img7 from "/src/images/series/serie7.png"
 
 
 const CardDetail2 = () => {
+
+  const {mode} = useAppContext();
+
   return (
     <>
-      <div>
-        <Back SectionName={'Series'} />
+    <div>
+      <Back SectionName={'Series'} />
+    </div>
+
+    <div className={`w-[98%] xl:w-[68%] xl:mx-auto md:mx-auto flex flex-col ${ mode ? 'text-[#ffffffc2]' : 'text-[#000000c2]'} z-50`} >
+      <DivLineCards />
+
+      <div className='flex xl:flex-row flex-col' >
+        <section className={SectionStyle} >
+          <PhotoMovie img={img1} />
+        </section>
+        <div className='flex flex-col text-center justify-center items-center gap-6' >
+          <h1 className={TitleStyle} >
+            Fauda
+          </h1>
+          <span className={DescriptionStyle} >
+            Un veterano agente israelí vuelve a la caza en busca de un luchador palestino que creía muerto, poniendo en marcha una serie de eventos caóticos.
+          </span>
+          <div className='flex flex-row text-xl' >
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStarHalfAlt />
+            <FaRegStar />
+          </div>
+          <a href='https://www.netflix.com/ar/title/80113612' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+            <FaPlayCircle />
+          </a>
+        </div>
       </div>
 
-      <div className='w-[98%] xl:w-[68%] mx-auto flex flex-col' >
       <DivLineCards />
-      <section className={SectionStyle} >
-        <PhotoMovie img={img2} />
-      </section>
+
+      <div className='flex xl:flex-row flex-col' >
+        <section className={SectionStyle} >
+          <PhotoMovie img={img2} />
+        </section>
+        <div className='flex flex-col text-center justify-center items-center gap-6' >
+          <h1 className={TitleStyle} >
+            East Side
+          </h1>
+          <span className={DescriptionStyle} >
+            Una serie basada en hechos reales que denuncia la ocupación de Jerusalén Este, de mayoría árabe, por parte de poderosos grupos israelíes.
+          </span>
+          <div className='flex flex-row text-xl' >
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStarHalfAlt />
+            <FaRegStar />
+          </div>
+          <a href='https://www.filmin.es/serie/east-side' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+            <FaPlayCircle />
+          </a>
+        </div>
+      </div>
+
       <DivLineCards />
-      <section className={SectionStyle} >
-        <PhotoMovie img={img1} />
-      </section>
+
+      <div className='flex xl:flex-row flex-col' >
+        <section className={SectionStyle} >
+          <PhotoMovie img={img3} />
+        </section>
+        <div className='flex flex-col text-center justify-center items-center gap-6' >
+          <h1 className={TitleStyle} >
+            Gaza Mon Amour
+          </h1>
+          <span className={DescriptionStyle} >
+          Narra un delicado romance entre dos ancianos en medio de la frontera del horror. Gaza, hoy.
+          </span>
+          <div className='flex flex-row text-xl' >
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStarHalfAlt />
+            <FaRegStar />
+          </div>
+          <a href='https://www.filmin.es/pelicula/gaza-mon-amour' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+            <FaPlayCircle />
+          </a>
+        </div>
+      </div>
+
       <DivLineCards />
-      <section className={SectionStyle} >
-        <PhotoMovie img={img3} />
-      </section>
+
+      <div className='flex xl:flex-row flex-col' >
+        <section className={SectionStyle} >
+          <PhotoMovie img={img4} />
+        </section>
+        <div className='flex flex-col text-center justify-center items-center gap-6' >
+          <h1 className={TitleStyle} >
+            Lo que Walaa quiere
+          </h1>
+          <span className={DescriptionStyle} >
+            Walaa, una chica adolescente criada en un campo de refugiados, que rompe todas las reglas al decidir convertirse en una de las pocas mujeres policías de las fuerzas de seguridad palestinas. 
+          </span>
+          <div className='flex flex-row text-xl' >
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaRegStar />
+            <FaRegStar />
+          </div>
+          <a href='https://www.filmin.es/pelicula/lo-que-walaa-quiere' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+            <FaPlayCircle />
+          </a>
+        </div>
+      </div>
+
       <DivLineCards />
-      <section className={SectionStyle} >
-        <PhotoMovie img={img4} />
-      </section>
+
+      <div className='flex xl:flex-row flex-col' >
+        <section className={SectionStyle} >
+          <PhotoMovie img={img5} />
+        </section>
+        <div className='flex flex-col text-center justify-center items-center gap-6' >
+          <h1 className={TitleStyle} >
+            GAZA
+          </h1>
+          <span className={DescriptionStyle} >
+            Diversos personajes muestran la vulneración de derechos humanos que sufre diariamente la población palestina en la Franja de Gaza y la situación de bloqueo y posguerra en la que trata de sobrevivir.
+          </span>
+          <div className='flex flex-row text-xl' >
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaRegStar />
+          </div>
+          <a href='https://www.filmin.es/corto/gaza' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+            <FaPlayCircle />
+          </a>
+        </div>
+      </div>
+
       <DivLineCards />
-      <section className={SectionStyle} >
-        <PhotoMovie img={img5} />
-      </section>
+
+      <div className='flex xl:flex-row flex-col' >
+        <section className={SectionStyle} >
+          <PhotoMovie img={img6} />
+        </section>
+        <div className='flex flex-col text-center justify-center items-center gap-6' >
+          <h1 className={TitleStyle} >
+            El color de los Olivos
+          </h1>
+          <span className={DescriptionStyle} >
+            La familia Amer vive rodeada por el muro en Cisjordania, puertas electrificadas, candados y la presencia constante de soldados. Se revelan sus luchas constantes y los pequeños detalles de su vida.
+          </span>
+          <div className='flex flex-row text-xl' >
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStarHalfAlt />
+          </div>
+          <a href='https://www.filmin.es/pelicula/el-color-de-los-olivos' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+            <FaPlayCircle />
+          </a>
+        </div>
+      </div>
+
       <DivLineCards />
-      <section className={SectionStyle} >
-        <PhotoMovie img={img6} />
-      </section>
+
+      <div className='flex xl:flex-row flex-col' >
+        <section className={SectionStyle} >
+          <PhotoMovie img={img7} />
+        </section>
+        <div className='flex flex-col text-center justify-center items-center gap-6' >
+          <h1 className={TitleStyle} >
+            Inch Allah
+          </h1>
+          <span className={DescriptionStyle} >
+            Chloe es una joven tocóloga que se ocupa de mujeres embarazadas bajo la supervisión de Michael, un médico francés, en un ambulatorio improvisado en un campo de refugiados de Cisjordania. Debe enfrentarse a diario a los controles y al conflicto que afectan a la vida de las personas.
+          </span>
+          <div className='flex flex-row text-xl' >
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaRegStar />
+            <FaRegStar />
+          </div>
+          <a href='https://www.filmin.es/pelicula/inchallah' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+            <FaPlayCircle />
+          </a>
+        </div>
+      </div>
+
       <DivLineCards />
-      <section className={SectionStyle} >
-        <PhotoMovie img={img7} />
-      </section>
-      <DivLineCards />
+
       <Footer />
     </div>
     </>
