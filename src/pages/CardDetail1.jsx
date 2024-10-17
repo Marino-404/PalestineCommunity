411e684144467260d28048ac31749a4f28b613fb
Juan Marino Rova <maarino89@hotmail.com>
2024-10-17 17:00:51 -0300

    1.0

--- src/pages/CardDetail1.jsx
+++ src/pages/CardDetail1.jsx
@@ -16,7 +16,8 @@ import { FaPlayCircle } from "react-icons/fa";
 
 export const SectionStyle = 'flex flex-col xl:flex-row justify-center items-center xl:justify-center xl:items-start md:items-start md:justify-start ';
 export const TitleStyle = 'font-poppins font-bold text-3xl pt-4'
-export const DescriptionStyle = 'font-poppins font-normal text-xl py-0 px-6'
+export const DescriptionStyle = 'font-poppins font-normal text-xl pb-4 px-6'
+export const Stars = 'flex flex-row text-xl pb-4'
 
 const CardDetail1 = () => {
 
@@ -36,14 +37,14 @@ const CardDetail1 = () => {
         <section className={SectionStyle} >
           <PhotoMovie img={img2} />
         </section>
-        <div className='flex flex-col text-center justify-center items-center gap-6' >
+        <div className='flex flex-col text-center justify-center items-center gap-2' >
           <h1 className={TitleStyle} >
             A 200 Metros
           </h1>
           <span className={DescriptionStyle} >
             Mustafa y su esposa Salwa provienen de dos aldeas palestinas que están a solo 200 metros de distancia, pero separadas por el muro. Su situación de vida inusual está comenzando a afectar su matrimonio feliz, pero la pareja hace lo que puede para que funcione. 
           </span>
-          <div className='flex flex-row text-xl' >
+          <div className={Stars} >
             <FaStar />
             <FaStar />
             <FaStar />
@@ -62,14 +63,14 @@ const CardDetail1 = () => {
         <section className={SectionStyle} >
           <PhotoMovie img={img1} />
         </section>
-        <div className='flex flex-col text-center justify-center items-center gap-6' >
+        <div className='flex flex-col text-center justify-center items-center gap-2' >
           <h1 className={TitleStyle} >
             La sal de este mar
           </h1>
           <span className={DescriptionStyle} >
             Soraya nació y se educó en Brooklyn, pero ha decidido regresar a Palestina, país del que su familia tuvo que exiliarse en 1948. Por su parte Emad, nacido en los territorios palestinos, ha conocido toda su vida la ocupación y la realidad palestina, y sueña con obtener un visado para ir a estudiar a Canadá y marcharse de allí.  
           </span>
-          <div className='flex flex-row text-xl' >
+          <div className={Stars} >
             <FaStar />
             <FaStar />
             <FaStar />
@@ -88,14 +89,14 @@ const CardDetail1 = () => {
         <section className={SectionStyle} >
           <PhotoMovie img={img3} />
         </section>
-        <div className='flex flex-col text-center justify-center items-center gap-6' >
+        <div className='flex flex-col text-center justify-center items-center gap-2' >
           <h1 className={TitleStyle} >
             Nacido en GAZA
           </h1>
           <span className={DescriptionStyle} >
             Rodada durante la última ofensiva israelí que asoló la Franja de Gaza, 2014. Nacido en Gaza sigue a diez niños que nos cuentan cómo es su día a día bajo las bombas y el embargo.
           </span>
-          <div className='flex flex-row text-xl' >
+          <div className={Stars} >
             <FaStar />
             <FaStar />
             <FaStar />
--- src/pages/CardDetail2.jsx
+++ src/pages/CardDetail2.jsx
@@ -4,7 +4,7 @@ import Back from '../assets/Back';
 import PhotoMovie from '../assets/PhotoMovie';
 import DivLinePages from '../assets/DivLinePages';
 import Footer from '../components/Footer';
-import { SectionStyle, TitleStyle, DescriptionStyle } from './CardDetail1';
+import { SectionStyle, TitleStyle, DescriptionStyle, Stars } from './CardDetail1';
 import { FaStarHalfAlt } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 import { FaRegStar } from "react-icons/fa";
@@ -36,14 +36,14 @@ const CardDetail2 = () => {
         <section className={SectionStyle} >
           <PhotoMovie img={img1} />
         </section>
-        <div className='flex flex-col text-center justify-center items-center gap-6' >
+        <div className='flex flex-col text-center justify-center items-center gap-2' >
           <h1 className={TitleStyle} >
             Fauda
           </h1>
           <span className={DescriptionStyle} >
             Un veterano agente israelí vuelve a la caza en busca de un luchador palestino que creía muerto, poniendo en marcha una serie de eventos caóticos.
           </span>
-          <div className='flex flex-row text-xl' >
+          <div className={Stars} >
             <FaStar />
             <FaStar />
             <FaStar />
@@ -62,14 +62,14 @@ const CardDetail2 = () => {
         <section className={SectionStyle} >
           <PhotoMovie img={img2} />
         </section>
-        <div className='flex flex-col text-center justify-center items-center gap-6' >
+        <div className='flex flex-col text-center justify-center items-center gap-2' >
           <h1 className={TitleStyle} >
             East Side
           </h1>
           <span className={DescriptionStyle} >
             Una serie basada en hechos reales que denuncia la ocupación de Jerusalén Este, de mayoría árabe, por parte de poderosos grupos israelíes.
           </span>
-          <div className='flex flex-row text-xl' >
+          <div className={Stars} >
             <FaStar />
             <FaStar />
             <FaStar />
@@ -88,14 +88,14 @@ const CardDetail2 = () => {
         <section className={SectionStyle} >
           <PhotoMovie img={img3} />
         </section>
-        <div className='flex flex-col text-center justify-center items-center gap-6' >
+        <div className='flex flex-col text-center justify-center items-center gap-2' >
           <h1 className={TitleStyle} >
             Gaza Mon Amour
           </h1>
           <span className={DescriptionStyle} >
           Narra un delicado romance entre dos ancianos en medio de la frontera del horror. Gaza, hoy.
           </span>
-          <div className='flex flex-row text-xl' >
+          <div className={Stars} >
             <FaStar />
             <FaStar />
             <FaStar />
@@ -114,14 +114,14 @@ const CardDetail2 = () => {
         <section className={SectionStyle} >
           <PhotoMovie img={img4} />
         </section>
-        <div className='flex flex-col text-center justify-center items-center gap-6' >
+        <div className='flex flex-col text-center justify-center items-center gap-2' >
           <h1 className={TitleStyle} >
             Lo que Walaa quiere
           </h1>
           <span className={DescriptionStyle} >
             Walaa, una chica adolescente criada en un campo de refugiados, que rompe todas las reglas al decidir convertirse en una de las pocas mujeres policías de las fuerzas de seguridad palestinas. 
           </span>
-          <div className='flex flex-row text-xl' >
+          <div className={Stars} >
             <FaStar />
             <FaStar />
             <FaStar />
@@ -140,14 +140,14 @@ const CardDetail2 = () => {
         <section className={SectionStyle} >
           <PhotoMovie img={img5} />
         </section>
-        <div className='flex flex-col text-center justify-center items-center gap-6' >
+        <div className='flex flex-col text-center justify-center items-center gap-2' >
           <h1 className={TitleStyle} >
             GAZA
           </h1>
           <span className={DescriptionStyle} >
             Diversos personajes muestran la vulneración de derechos humanos que sufre diariamente la población palestina en la Franja de Gaza y la situación de bloqueo y posguerra en la que trata de sobrevivir.
           </span>
-          <div className='flex flex-row text-xl' >
+          <div className={Stars} >
             <FaStar />
             <FaStar />
             <FaStar />
@@ -166,14 +166,14 @@ const CardDetail2 = () => {
         <section className={SectionStyle} >
           <PhotoMovie img={img6} />
         </section>
-        <div className='flex flex-col text-center justify-center items-center gap-6' >
+        <div className='flex flex-col text-center justify-center items-center gap-2' >
           <h1 className={TitleStyle} >
             El color de los Olivos
           </h1>
           <span className={DescriptionStyle} >
             La familia Amer vive rodeada por el muro en Cisjordania, puertas electrificadas, candados y la presencia constante de soldados. Se revelan sus luchas constantes y los pequeños detalles de su vida.
           </span>
-          <div className='flex flex-row text-xl' >
+          <div className={Stars} >
             <FaStar />
             <FaStar />
             <FaStar />
@@ -192,14 +192,14 @@ const CardDetail2 = () => {
         <section className={SectionStyle} >
           <PhotoMovie img={img7} />
         </section>
-        <div className='flex flex-col text-center justify-center items-center gap-6' >
+        <div className='flex flex-col text-center justify-center items-center gap-2' >
           <h1 className={TitleStyle} >
             Inch Allah
           </h1>
           <span className={DescriptionStyle} >
             Chloe es una joven tocóloga que se ocupa de mujeres embarazadas bajo la supervisión de Michael, un médico francés, en un ambulatorio improvisado en un campo de refugiados de Cisjordania. Debe enfrentarse a diario a los controles y al conflicto que afectan a la vida de las personas.
           </span>
-          <div className='flex flex-row text-xl' >
+          <div className={Stars} >
             <FaStar />
             <FaStar />
             <FaStar />
