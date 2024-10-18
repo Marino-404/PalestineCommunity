1775514c30446039e6ea5530f02b2260f8b95a25
Juan Marino Rova <maarino89@hotmail.com>
2024-10-18 10:05:05 -0300

    1.0

--- src/assets/Logo.jsx
+++ src/assets/Logo.jsx
@@ -4,7 +4,7 @@ import heroImg from "/src/images/Logo.png";
 function Logo() {
     return (
         <div className="flex justify-center items-center ">
-            <img src={heroImg} alt="logo" className="xl:w-[230px] xl:h-[220px] h-[180px] z-10 items-center justify-center hover:animate-spin hover:animate-once animate-duration-[1000ms] animate-delay-[500ms] animate-ease-in-out" />
+            <img src={heroImg} alt="logo" className="xl:w-[230px] xl:h-[220px] h-[180px] z-10 items-center justify-center hover:animate-spin animate-once animate-duration-[1000ms] animate-delay-0 animate-ease-in-out" />
         </div>
     )
 }
