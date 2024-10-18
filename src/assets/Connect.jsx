8feb59c150a5cbb8c5a9ad6a6210d1bb91b7acd2
Juan Marino Rova <maarino89@hotmail.com>
2024-10-18 11:36:50 -0300

    1.0

--- src/assets/Connect.jsx
+++ src/assets/Connect.jsx
@@ -19,7 +19,7 @@ const Connect = () => {
     const { mode } = useAppContext();
 
     return (
-      <div className={`flex items-center w-[98%] xl:w-[68%] justify-center xl:gap-6 my-6 xl:my-2 ${mode ? 'bg-[#00000097]' : 'bg-[#ffffff97]'}`}>
+      <div className={`flex items-center w-[98%] xl:w-[68%] justify-center gap-1 xl:gap-6 my-6 xl:my-2 ${mode ? 'bg-[#00000097]' : 'bg-[#ffffff97]'}`}>
 
         <Link to={'/cards/detail7'}>
           <BsFillPeopleFill className={`text-3xl xl:text-6xl text-[#247043] ${mode ? 'opacity-30' : 'opacity-40'}`} />
