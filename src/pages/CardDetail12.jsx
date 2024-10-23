ad128babb7c62c9207ac9c052efb019c1f599862
Juan Marino Rova <maarino89@hotmail.com>
2024-10-23 20:08:36 -0300

    1.0

--- src/pages/CardDetail12.jsx
+++ src/pages/CardDetail12.jsx
@@ -6,8 +6,8 @@ import Footer from '../components/Footer';
 import { AiFillInstagram } from "react-icons/ai";
 import { FaYoutube } from 'react-icons/fa';
 
-const TitleStyle = 'font-poppins font-semibold text-2xl mb-4'
-const UbicationStyle = 'font-poppins font-bold text-3xl'
+const TitleStyle = 'font-poppins font-semibold text-2xl'
+const UbicationStyle = 'font-poppins font-bold text-3xl mb-4 underline'
 
 const CardDetail12 = () => {
 
@@ -23,11 +23,14 @@ const CardDetail12 = () => {
       
         <DivLinePages />
 
-        <div className='xl:h-[20vw] flex xl:flex-row flex-col' >
+        <div className='xl:h-[15vw] flex xl:flex-row flex-col' >
           <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
             <h1 className={TitleStyle} >
-              EMBAJADA DE PALESTINA <span className={UbicationStyle}>EN ARGENTINA</span>
+              EMBAJADA DE PALESTINA
             </h1>
+            <span className={UbicationStyle} >
+              ARGENTINA 
+            </span>
             <div className='flex gap-4' >
               <a href='https://www.instagram.com/palestinaenargentina?igsh=MXJ5aXo2d2RxbzdhZA==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
                 <AiFillInstagram />
@@ -41,92 +44,97 @@ const CardDetail12 = () => {
 
         <DivLinePages />
 
-        <div className='xl:h-[20vw] flex xl:flex-row flex-col' >
+        <div className='xl:h-[15vw] flex xl:flex-row flex-col' >
           <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
             <h1 className={TitleStyle} >
-              EMBAJADA DE PALESTINA EN PERU
+              EMBAJADA DE PALESTINA
             </h1>
-              <a href='https://www.instagram.com/palestinaenperu?igsh=MWxqZm45dXZ3a3o2Nw==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
-                <AiFillInstagram />
-              </a> 
+            <span className={UbicationStyle} >
+              PERU 
+            </span>
+            <a href='https://www.instagram.com/palestinaenperu?igsh=MWxqZm45dXZ3a3o2Nw==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+              <AiFillInstagram />
+            </a> 
           </div>
         </div>
 
         <DivLinePages />
 
-        <div className='xl:h-[20vw] flex xl:flex-row flex-col' >
+        <div className='xl:h-[15vw] flex xl:flex-row flex-col' >
           <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
             <h1 className={TitleStyle} >
-              EMBAJADA DE PALESTINA EN CHILE
+              EMBAJADA DE PALESTINA
             </h1>
-              <a href='https://www.instagram.com/embajadapalestinachile?igsh=MWdtZTdzenIzbHo2Yw==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
-                <AiFillInstagram />
-              </a> 
+            <span className={UbicationStyle} >
+              CHILE
+            </span>
+            <a href='https://www.instagram.com/embajadapalestinachile?igsh=MWdtZTdzenIzbHo2Yw==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+              <AiFillInstagram />
+            </a> 
           </div>
         </div>
 
         <DivLinePages />
 
-        <div className='xl:h-[20vw] flex xl:flex-row flex-col' >
+        <div className='xl:h-[15vw] flex xl:flex-row flex-col' >
           <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
             <h1 className={TitleStyle} >
-              EMBAJADA DE PALESTINA EN URUGUAY
+              EMBAJADA DE PALESTINA
             </h1>
-              <a href='https://www.instagram.com/palestinaenuruguay?igsh=MTB1aGdlZGppaXpmeA==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
-                <AiFillInstagram />
-              </a> 
+            <span className={UbicationStyle} >
+              URUGUAY
+            </span>
+            <a href='https://www.instagram.com/palestinaenuruguay?igsh=MTB1aGdlZGppaXpmeA==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+              <AiFillInstagram />
+            </a> 
           </div>
         </div>
 
         <DivLinePages />
 
-        <div className='xl:h-[20vw] flex xl:flex-row flex-col' >
+        <div className='xl:h-[15vw] flex xl:flex-row flex-col' >
           <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
             <h1 className={TitleStyle} >
-              EMBAJADA DE PALESTINA EN NICARAGUA
+              EMBAJADA DE PALESTINA
             </h1>
-              <a href='https://www.instagram.com/palestinaennicaragua?igsh=MTFzMHJxeGFtcGliaA==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
-                <AiFillInstagram />
-              </a> 
+            <span className={UbicationStyle} >
+              NICARAGUA
+            </span>
+            <a href='https://www.instagram.com/palestinaennicaragua?igsh=MTFzMHJxeGFtcGliaA==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+              <AiFillInstagram />
+            </a> 
           </div>
         </div>
 
         <DivLinePages />
 
-        <div className='xl:h-[20vw] flex xl:flex-row flex-col' >
+        <div className='xl:h-[15vw] flex xl:flex-row flex-col' >
           <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
             <h1 className={TitleStyle} >
-              EMBAJADA DE PALESTINA EN PERU
+              EMBAJADA DE PALESTINA
             </h1>
-              <a href='https://www.instagram.com/palestinaenargentina?igsh=MXJ5aXo2d2RxbzdhZA==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
-                <AiFillInstagram />
-              </a> 
+            <span className={UbicationStyle} >
+              COLOMBIA
+            </span>
+            <a href='https://www.instagram.com/embpalcol?igsh=MWo5NHZleDF4OXRnaA==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+              <AiFillInstagram />
+            </a> 
           </div>
         </div>
 
         <DivLinePages />
 
-        <div className='xl:h-[20vw] flex xl:flex-row flex-col' >
+        <div className='xl:h-[15vw] flex xl:flex-row flex-col' >
           <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
             <h1 className={TitleStyle} >
-              EMBAJADA DE PALESTINA EN COLOMBIA
+              EMBAJADA DE PALESTINA
             </h1>
-              <a href='https://www.instagram.com/embpalcol?igsh=MWo5NHZleDF4OXRnaA==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
-                <AiFillInstagram />
-              </a> 
-          </div>
-        </div>
-
-        <DivLinePages />
-
-        <div className='xl:h-[20vw] flex xl:flex-row flex-col' >
-          <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
-            <h1 className={TitleStyle} >
-              EMBAJADA DE PALESTINA EN ESPAÑA
-            </h1>
-              <a href='https://www.instagram.com/palestinaenespana?igsh=NDY1NHoycHVha2Ry' target="_blank" rel="noopener noreferrer" className='text-5xl' >
-                <AiFillInstagram />
-              </a> 
+            <span className={UbicationStyle} >
+              ESPAÑA
+            </span>
+            <a href='https://www.instagram.com/palestinaenespana?igsh=NDY1NHoycHVha2Ry' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+              <AiFillInstagram />
+            </a> 
           </div>
         </div>
 
