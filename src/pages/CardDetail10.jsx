aff84669a2b48bd449c502b4bad7e90caf16d006
Juan Marino Rova <maarino89@hotmail.com>
2024-10-24 16:02:48 -0300

    1.0

--- src/pages/CardDetail10.jsx
+++ src/pages/CardDetail10.jsx
@@ -1,11 +1,97 @@
 import React from 'react';
+import { useAppContext } from '../AppContext';
 import Back from '../assets/Back';
+import DivLinePages from '../assets/DivLinePages';
+import Footer from '../components/Footer';
+import { AiFillInstagram } from "react-icons/ai";
+import { BiWorld } from 'react-icons/bi';
+
+const TitleStyle = 'font-poppins font-semibold text-2xl text-gradient'
 
 const CardDetail10 = () => {
+
+  const { mode } = useAppContext();
+
   return (
-    <div>
-      <Back SectionName={'ONG'} />
-    </div>
+    <>
+      <div>
+        <Back SectionName={'ONG'} />
+      </div>
+
+      <div className={`w-[98%] xl:w-[50%] xl:mx-auto md:mx-auto flex flex-col ${ mode ? 'text-custom-white' : 'text-custom-black'} z-50`} >
+      
+        <DivLinePages />
+
+        <div className='xl:h-[18vw] flex xl:flex-row flex-col' >
+          <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
+            <h1 className={TitleStyle} >
+              YASSSER ARAFAT FOUNDATION
+            </h1>
+              <a href='https://www.yaf.ps/index-en.html' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+                <BiWorld />
+              </a> 
+          </div>
+        </div>
+
+        <DivLinePages />
+
+        <div className='xl:h-[18vw] flex xl:flex-row flex-col' >
+          <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
+            <h1 className={TitleStyle} >
+              FUNDACION BELEN 
+            </h1>
+            <a href='https://www.instagram.com/fundacionbelen2000?igsh=MTA0amk5MnU2YW84ZQ==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+              <BiWorld />
+            </a> 
+          </div>
+        </div>
+
+        <DivLinePages />
+
+        <div className='xl:h-[18vw] flex xl:flex-row flex-col' >
+          <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
+            <h1 className={TitleStyle} >
+              FUNDACION SIRIO PALESTINA
+            </h1>
+            <a href='https://www.instagram.com/fundacionsiriopalestina?igsh=MTE2c3k0OXN4bDI2OQ==' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+              <AiFillInstagram />
+            </a> 
+          </div>
+        </div>
+
+        <DivLinePages />
+
+        <div className='xl:h-[18vw] flex xl:flex-row flex-col' >
+          <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
+            <h1 className={TitleStyle} >
+              DELIA ART FOUNDATION
+            </h1>
+            <a href='https://www.instagram.com/delia.arts.foundation?igsh=cnM3b2hqYWJqeTlr' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+              <AiFillInstagram />
+            </a> 
+          </div>
+        </div>
+
+        <DivLinePages />
+
+        <div className='xl:h-[18vw] flex xl:flex-row flex-col' >
+          <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
+            <h1 className={TitleStyle} >
+              QATTAN FOUNDATION
+            </h1>
+            <a href='https://www.instagram.com/qattanfoundation?igsh=aHJ4OWxvaG14dTY5' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+              <AiFillInstagram />
+            </a> 
+          </div>
+        </div>
+
+        <DivLinePages />
+
+        <Footer />
+         
+      </div>
+
+    </>
   );
 };
 
--- src/pages/CardDetail11.jsx
+++ src/pages/CardDetail11.jsx
@@ -1,12 +1,60 @@
 import React from 'react';
+import { useAppContext } from '../AppContext';
 import Back from '../assets/Back';
+import DivLinePages from '../assets/DivLinePages';
+import Footer from '../components/Footer';
+import { FaYoutube } from 'react-icons/fa';
 
-const CardDetail1 = () => {
+const TitleStyle = 'font-poppins font-semibold text-2xl text-gradient'
+const UbicationStyle = 'font-poppins font-bold text-3xl mb-4 '
+
+const CardDetail11 = () => {
+
+  const { mode } = useAppContext();
+
   return (
-    <div>
-      <Back SectionName={'Turismo'} />
-    </div>
+    <>
+      <div>
+        <Back SectionName={'Turismo'} />
+      </div>
+
+      <div className={`w-[98%] xl:w-[50%] xl:mx-auto md:mx-auto flex flex-col ${ mode ? 'text-custom-white' : 'text-custom-black'} z-50`} >
+      
+        <DivLinePages />
+
+        <div className='xl:h-[18vw] flex xl:flex-row flex-col' >
+          <div className='flex flex-col w-full text-center justify-center items-center gap-2' >
+            <h1 className={TitleStyle} >
+              Turismo en Palestina
+            </h1>
+            <span className={UbicationStyle} >
+              Videos:
+            </span>
+            <div className='flex gap-4' >
+              <a href='https://www.youtube.com/watch?v=QDkeiezIOdY' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+                <FaYoutube />
+              </a> 
+              <a href='https://www.youtube.com/watch?v=arfeUUdAZHY' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+                <FaYoutube />
+              </a>
+              <a href='https://www.youtube.com/watch?v=MJ339LbuByM' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+                <FaYoutube />
+              </a>
+              <a href='https://www.youtube.com/watch?v=v8ACR1Jn9k4' target="_blank" rel="noopener noreferrer" className='text-5xl' >
+                <FaYoutube />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <DivLinePages />
+
+        <Footer />
+         
+      </div>
+
+    </>
   );
 };
 
-export default CardDetail1;
+export default CardDetail11;
