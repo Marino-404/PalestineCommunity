8875137cf133da60d9bc29e3d030a91db9d8055c
Juan Marino Rova <maarino89@hotmail.com>
2024-10-26 20:47:58 -0300

    1.0

--- src/components/ButtonFixedContact.jsx
+++ src/components/ButtonFixedContact.jsx
@@ -6,7 +6,7 @@ import { FiMessageCircle } from "react-icons/fi";
 const ButtonFixedContact = ({ onClick }) => {
     return (
         <a 
-        href="#Community" 
+        href="#contact" 
         onClick={onClick} 
         className="fixed rounded-full flex items-center justify-center w-[58px] h-[58px] bg-gradient-to-b from-[#247043] to-[#778F43] text-custom-white bottom-[8vw] right-[6vw]  md:bottom-[3vw] md:right-[3vw] z-50 opacity-90" >
             <FiMessageCircle className="text-custom-white text-3xl" />
