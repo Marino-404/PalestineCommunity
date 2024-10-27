f50aeb31f29512bc9fd3fdc71ae21f481f1ab464
Juan Marino Rova <maarino89@hotmail.com>
2024-10-27 11:06:32 -0300

    1.0

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -37,7 +37,9 @@ const Header = () => {
   return (
     <>
     <header  className={`xl:fixed flex items-center justify-between w-full h-[7vh] xl:h-[6vh] z-50 ${mode ? "text-custom-white" : "text-custom-black"} xl:animate-fade-down animate-duration-[1000ms]`}>
-      <nav className={`fixed flex-1 text-center w-full h-full ${mode ? "text-custom-white" : "text-custom-black"} ${showMenu ? "top-0" : "-top-full"} ${mode ? "bg-[#000000e7]" : "bg-[#ffffffea]"} ${mode ? "xl:bg-[#000000cc]" : "xl:bg-[#ffffffef]"} transition-all duration-500 xl:transition-none xl:static flex flex-col xl:flex-row items-center justify-center gap-10 xl:gap-28 z-50`}>
+      <nav 
+        className={`fixed flex-1 text-center w-full h-full ${mode ? "text-custom-white" : "text-custom-black"} ${showMenu ? "top-0" : "-top-full"} ${mode ? "bg-[#000000e7]" : "bg-[#ffffffea]"} ${mode ? "xl:bg-[#000000cc]" : "xl:bg-[#ffffffef]"} transition-all duration-500 xl:transition-none xl:static flex flex-col xl:flex-row items-center justify-center gap-10 xl:gap-28 z-50`}
+        onClick={() => setShowMenu(!showMenu)}>
         <a className={HeaderStyle} href="#Connect" onClick={() => handleMenuItemClick('connect')}>Connect</a>
         <a className={HeaderStyle} href="#About" onClick={() => handleMenuItemClick('about')}>Sobre Nosotros</a>
         <a className={HeaderStyle} href="#Projects" onClick={() => handleMenuItemClick('projects')}>Proyectos</a>
@@ -45,13 +47,13 @@ const Header = () => {
         <div 
             className={` ${ showMenu ? "block" : "hidden xl:block"} flex flex-col items-center pt-10 cursor-default`}>
             <AiFillMoon
-              onClick={toggleMode}
+              onClick={(e) => {toggleMode(); e.stopPropagation();}}
               className={`${
                 !mode ? "bottom-[200px] xl:top-[12px]" : "bottom-[-400px] xl:top-[-150px]"
                 } cursor-pointer transition-all duration-100 xl:duration-300 absolute w-[30px] h-[30px] xl:h-[20px] xl:w-[20px]`} 
               />
             <AiFillSun
-              onClick={toggleMode}
+              onClick={(e) => {toggleMode(); e.stopPropagation();}}
               className={`${
                 mode ? "bottom-[200px] xl:top-[12px]" : "bottom-[-400px] xl:top-[-150px]"
                 } cursor-pointer transition-all duration-100 xl:duration-300 absolute w-[30px] h-[30px] xl:h-[20px] xl:w-[20px]`} 
