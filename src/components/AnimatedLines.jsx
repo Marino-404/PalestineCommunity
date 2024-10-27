a64f83550c09175125e252763c24a5bb2680c3d5
Juan Marino Rova <maarino89@hotmail.com>
2024-10-27 10:31:30 -0300

    1.0

--- src/components/AnimatedLines.jsx
+++ src/components/AnimatedLines.jsx
@@ -1,27 +1,26 @@
-import React, { memo } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const AnimatedLines = memo(() => {
+const AnimatedLines = () => {
+    const [isMoving, setIsMoving] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsMoving((prev) => !prev); 
+        }, 3000); 
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="fixed z-0 opacity-20">
-            <div
-                className="fixed infiniteMove4 w-2 rounded h-screen top-0 right-[10vw]"
-                style={{ background: '#247043' }}
-            ></div>
-            <div
-                className="fixed infiniteMove w-2 rounded h-screen top-0 right-[15vw]"
-                style={{ background: '#247043' }}
-            ></div>
-            <div
-                className="fixed infiniteMove2 w-2 rounded h-screen top-0 left-[10vw]"
-                style={{ background: '#247043' }}
-            ></div>
-            <div
-                className="fixed infiniteMove3 w-2 rounded h-screen top-0 left-[15vw]"
-                style={{ background: '#247043' }}
-            ></div>
+            <div className={`line line1 ${isMoving ? 'moving' : ''}`}></div>
+            <div className={`line line2 ${isMoving ? 'moving' : ''}`}></div>
+            <div className={`line line3 ${isMoving ? 'moving' : ''}`}></div>
+            <div className={`line line4 ${isMoving ? 'moving' : ''}`}></div>
         </div>
     );
-});
+};
 
 export default AnimatedLines;
+
 
