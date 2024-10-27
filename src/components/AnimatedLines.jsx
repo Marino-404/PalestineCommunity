822946882b8a38bec3bfdcebfadacf3d1f00eb20
Juan Marino Rova <maarino89@hotmail.com>
2024-10-27 10:21:24 -0300

    1.0

--- src/components/AnimatedLines.jsx
+++ src/components/AnimatedLines.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { memo } from 'react';
 
-const AnimatedLine = ({ className, position }) => (
-    <div
-        className={`fixed ${className} w-2 rounded h-screen ${position}`}
-        style={{ background: '#247043' }}
-    ></div>
-);
-
-function AnimatedLines() {
+const AnimatedLines = memo(() => {
     return (
         <div className="fixed z-0 opacity-20">
-            <AnimatedLine className="infiniteMove4" position="top-0 right-[10vw]" />
-            <AnimatedLine className="infiniteMove" position="top-0 right-[15vw]" />
-            <AnimatedLine className="infiniteMove2" position="top-0 left-[10vw]" />
-            <AnimatedLine className="infiniteMove3" position="top-0 left-[15vw]" />
+            <div
+                className="fixed infiniteMove4 w-2 rounded h-screen top-0 right-[10vw]"
+                style={{ background: '#247043' }}
+            ></div>
+            <div
+                className="fixed infiniteMove w-2 rounded h-screen top-0 right-[15vw]"
+                style={{ background: '#247043' }}
+            ></div>
+            <div
+                className="fixed infiniteMove2 w-2 rounded h-screen top-0 left-[10vw]"
+                style={{ background: '#247043' }}
+            ></div>
+            <div
+                className="fixed infiniteMove3 w-2 rounded h-screen top-0 left-[15vw]"
+                style={{ background: '#247043' }}
+            ></div>
         </div>
     );
-}
+});
 
 export default AnimatedLines;
+
