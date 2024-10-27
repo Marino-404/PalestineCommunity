87fd664b31f1b7f71282cadd84ff13517617b410
Juan Marino Rova <maarino89@hotmail.com>
2024-10-27 10:38:35 -0300

    1.0

--- src/components/AnimatedLines.jsx
+++ src/components/AnimatedLines.jsx
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
 const AnimatedLines = () => {
-    const [isMoving, setIsMoving] = useState(false);
+    const [movingLines, setMovingLines] = useState([false, false, false, false]);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setIsMoving((prev) => !prev); 
-        }, 3000); 
+            setMovingLines((prev) => 
+                prev.map((moving, index) => !moving) 
+            );
+        }, 1500); 
 
         return () => clearInterval(interval);
     }, []);
 
     return (
         <div className="fixed z-0 opacity-20">
-            <div className={`line line1 ${isMoving ? 'moving' : ''}`}></div>
-            <div className={`line line2 ${isMoving ? 'moving' : ''}`}></div>
-            <div className={`line line3 ${isMoving ? 'moving' : ''}`}></div>
-            <div className={`line line4 ${isMoving ? 'moving' : ''}`}></div>
+            <div className={`line line1 ${movingLines[0] ? 'moving' : ''}`}></div>
+            <div className={`line line2 ${movingLines[1] ? 'moving' : ''}`}></div>
+            <div className={`line line3 ${movingLines[2] ? 'moving' : ''}`}></div>
+            <div className={`line line4 ${movingLines[3] ? 'moving' : ''}`}></div>
         </div>
     );
 };
 
 export default AnimatedLines;
+
 
 
