b8fc88f93dead9204cd45e03756f762a90ff3c9a
Juan Marino Rova <maarino89@hotmail.com>
2024-11-07 09:11:41 -0300

    1.0

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useAppContext } from "../AppContext";
 
 const Button = () => {
+  const { mode } = useAppContext();
+
   return (
     <button
       type="submit"
-      className="text-sm font-poppins font-normal py-2 px-6 bg-gradient-to-b from-[#247043] to-[#778F43] text-white dark:text-black rounded-lg"
+      className={`text-sm font-poppins font-normal py-2 px-6 bg-gradient-to-b from-[#247043] to-[#778F43] ${
+        mode ? "text-custom-black" : "text-custom-white"
+      } rounded-lg`}
     >
       Enviar
     </button>
